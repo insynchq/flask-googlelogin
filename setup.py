c90b1432188d3fb750623455bf89f3e39c0cab42
Mark Steve Samson <hello@marksteve.com>
2013-03-08 09:55:05 +0800

    Bump version number

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools import setup
 
 setup(
     name='Flask-GoogleLogin',
-    version='0.1.1',
+    version='0.1.2',
     url='https://github.com/marksteve/flask-googlelogin',
     license='MIT',
     author="Mark Steve Samson",

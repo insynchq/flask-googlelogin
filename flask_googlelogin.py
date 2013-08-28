93bc102be405e8ec91b02187e45a1403a8fb1088
Mark Steve Samson <hello@marksteve.com>
2013-08-28 14:09:54 +0800

    Add browser flow

--- flask_googlelogin.py
+++ flask_googlelogin.py
@@ -21,7 +21,9 @@ USERINFO_PROFILE_SCOPE = 'https://www.googleapis.com/auth/userinfo.profile'
 
 
 class GoogleLogin(object):
-    """Main extension class"""
+    """
+    Main extension class
+    """
 
     def __init__(self, app=None, login_manager=None):
         if login_manager:
@@ -34,8 +36,10 @@ class GoogleLogin(object):
             self.init_app(app)
 
     def init_app(self, app, add_context_processor=True, login_manager=None):
-        """Initialize with app configuration. Existing
-        `flask_login.LoginManager` instance can be passed."""
+        """
+        Initialize with app configuration. Existing
+        `flask_login.LoginManager` instance can be passed.
+        """
 
         if login_manager:
             self.login_manager = login_manager
@@ -75,8 +79,16 @@ class GoogleLogin(object):
     def redirect_uri(self):
         return self.app.config.get('GOOGLE_LOGIN_REDIRECT_URI')
 
+    def sign_params(self, params):
+        return b64encode(urlencode(dict(sig=make_secure_token(**params),
+                                        **params)))
+
+    def parse_state(self, state):
+        return dict(parse_qsl(b64decode(str(state))))
+
     def login_url(self, params=None, **kwargs):
-        """Return login url with params encoded in state
+        """
+        Return login url with params encoded in state
 
         Available Google auth server params:
         response_type: code, token
@@ -87,9 +99,6 @@ class GoogleLogin(object):
         redirect_uri: string
         login_hint: string
         """
-        if not params:
-            params = {}
-
         kwargs.setdefault('response_type', 'code')
         kwargs.setdefault('access_type', 'online')
 
@@ -100,26 +109,26 @@ class GoogleLogin(object):
         if USERINFO_PROFILE_SCOPE not in scopes:
             scopes.append(USERINFO_PROFILE_SCOPE)
 
-        # NOTE: redirect_uri is stored in state for use later in getting token
-        params['redirect_uri'] = kwargs.pop('redirect_uri', self.redirect_uri)
-
-        state = b64encode(urlencode(dict(sig=make_secure_token(**params),
-                                         **params)))
+        redirect_uri = kwargs.pop('redirect_uri', self.redirect_uri)
+        state = self.sign_params(params)
 
         return GOOGLE_OAUTH2_AUTH_URL + '?' + urlencode(
             dict(client_id=self.client_id,
                  scope=' '.join(scopes),
-                 redirect_uri=params['redirect_uri'],
+                 redirect_uri=redirect_uri,
                  state=state,
                  **kwargs))
 
     def unauthorized_callback(self):
-        """Redirect to login url with next param set as request.url"""
+        """
+        Redirect to login url with next param set as request.url
+        """
         return redirect(self.login_url(params=dict(next=request.url)))
 
-    def login(self, code, redirect_uri):
-        """Exchanges code for tokens and returns retrieved `userinfo` and
-        `token`"""
+    def exchange_code(self, code, redirect_uri):
+        """
+        Exchanges code for tokens and returns retrieved `userinfo` and `token`
+        """
 
         token = requests.post(GOOGLE_OAUTH2_TOKEN_URL, data=dict(
             code=code,
@@ -130,17 +139,20 @@ class GoogleLogin(object):
         )).json
         if not token or token.get('error'):
             abort(400)
+        return token
 
+    def get_userinfo(self, access_token):
         userinfo = requests.get(GOOGLE_OAUTH2_USERINFO_URL, params=dict(
-            access_token=token['access_token'],
+            access_token=access_token,
         )).json
         if not userinfo or userinfo.get('error'):
             abort(400)
-
-        return token, userinfo
+        return userinfo
 
     def get_access_token(self, refresh_token):
-        """Use a refresh token to obtain a new access token"""
+        """
+        Use a refresh token to obtain a new access token
+        """
 
         token = requests.post(GOOGLE_OAUTH2_TOKEN_URL, data=dict(
             refresh_token=refresh_token,
@@ -155,30 +167,48 @@ class GoogleLogin(object):
         return token
 
     def oauth2callback(self, view_func):
-        """Decorator for OAuth2 callback. Calls `GoogleLogin.login` then
-        passes results to `view_func`."""
+        """
+        Decorator for OAuth2 callback. Calls `GoogleLogin.login` then
+        passes results to `view_func`.
+        """
 
         @wraps(view_func)
         def decorated(*args, **kwargs):
-            code = request.args.get('code')
-            if not code:
-                abort(400)
+            params = {}
 
             # Check sig
-            params = dict(parse_qsl(b64decode(str(request.args.get('state')))))
-            if params.pop('sig', None) != make_secure_token(**params):
-                return self.login_manager.unauthorized()
+            if 'state' in request.args:
+                params.update(**self.parse_state(request.args.get('state')))
+                if params.pop('sig', None) != make_secure_token(**params):
+                    return self.login_manager.unauthorized()
 
-            # Get userinfo and token
-            token, userinfo = self.login(code, params.pop('redirect_uri'))
+            code = request.args.get('code')
 
-            params.update(token=token, userinfo=userinfo)
+            # Web server flow
+            if code:
+                token = self.exchange_code(code, request.url)
+                userinfo = self.get_userinfo(token['access_token'])
+                params.update(token=token, userinfo=userinfo)
+
+            # Browser flow
+            else:
+                if params:
+                    params.update(dict(request.args.items()))
+                else:
+                    return '''
+                    <script>
+                      window.onload = function() {
+                        location.href = '?' + window.location.hash.substr(1);
+                      };
+                    </script>
+                    '''
 
             return view_func(**params)
 
         return decorated
 
     def user_loader(self, func):
-        """Shortcut to `login_manager`'s
-        `flask_login.LoginManager.user_loader`"""
+        """
+        Shortcut for `login_manager`'s `flask_login.LoginManager.user_loader`
+        """
         self.login_manager.user_loader(func)
